6567e6cae302bab1e10845d6b85d90233d0888f5
codgi <350247288@qq.com>
2020-11-25 20:19:40 +0800

    :lipstick: add Footer.vue but not finish

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.footer
+  .center
+    p Wenote
+    p Created By Sheey&T-Fate&Codgi
+</template>
+
+
+<style scoped>
+/* can not set the height */
+.footer {
+  padding: 10px;
+  height: 30px;
+}
+.center{
+  text-align: center;
+  font-size: 15px;
+}
+</style>
