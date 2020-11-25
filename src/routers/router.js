6c3360471daabd0850813fc94e8d3c63fd7a2b2f
codgi <350247288@qq.com>
2020-11-25 20:22:17 +0800

    :lipstick: add routers

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,16 +4,18 @@ import VueRouter from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Login from '@/views/Login.vue'
 import Note from '@/views/Note.vue'
-import Index from '@/components/Index.vue'
+import Index from '@/views/Index.vue'
 
 
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: HelloWorld },
+    { path: '/home', component: HelloWorld },
     { path: '/login', component: Login },
     { path: '/note/:id', component: Note },
-    { path: '/index', component: Index }
+    { path: '/index', component: Index },
+    { path: '/about', redirect: '/note/5fb922d85703375b1ccf28dc' },
+    { path: '/problem', redirect: 'note/5fbe48fb43facc04e2ddd7a7' },
 ]
 
 export default new VueRouter({
