9383325f12cc8d2f3dbbe4358f53df2c4691ce8d
codgi <350247288@qq.com>
2020-11-29 17:35:05 +0800

    :sparkles: Add 404 router

--- src/routers/router.js
+++ src/routers/router.js
@@ -6,7 +6,7 @@ import Login from '@/views/Login.vue'
 import Note from '@/views/Note.vue'
 import Index from '@/views/Index.vue'
 import Register from '@/views/Register.vue'
-
+import PageNotFound from '@/views/404.vue'
 Vue.use(VueRouter);
 
 const routes = [
@@ -16,7 +16,9 @@ const routes = [
     { path: '/index', component: Index },
     { path: '/about', redirect: '/note/5fb922d85703375b1ccf28dc' },
     { path: '/problem', redirect: 'note/5fbe48fb43facc04e2ddd7a7' },
-    { path: '/register', component: Register }
+    { path: '/register', component: Register },
+    { path: '/404', component: PageNotFound },
+    { path: '*', redirect: '/404' }
 ]
 
 export default new VueRouter({
