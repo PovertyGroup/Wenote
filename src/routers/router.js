5d0053d7eeef629367fc945542c1e66bf02e9a57
codgi <350247288@qq.com>
2020-11-30 16:08:04 +0800

    :children_crossing: redirect / to /index

--- src/routers/router.js
+++ src/routers/router.js
@@ -7,6 +7,7 @@ import Note from '@/views/Note.vue'
 import Index from '@/views/Index.vue'
 import Register from '@/views/Register.vue'
 import PageNotFound from '@/views/404.vue'
+import Info from '@/views/Info.vue'
 Vue.use(VueRouter);
 
 const routes = [
@@ -14,11 +15,13 @@ const routes = [
     { path: '/login', component: Login },
     { path: '/note/:id', component: Note },
     { path: '/index', component: Index },
+    { path: '/', redirect: '/index' },
     { path: '/about', redirect: '/note/5fb922d85703375b1ccf28dc' },
     { path: '/problem', redirect: 'note/5fbe48fb43facc04e2ddd7a7' },
     { path: '/register', component: Register },
+    { path: '/info', component: Info },
     { path: '/404', component: PageNotFound },
-    { path: '*', redirect: '/404' }
+    { path: '*', redirect: '/404' },
 ]
 
 export default new VueRouter({
