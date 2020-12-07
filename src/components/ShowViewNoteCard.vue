c64c32b7324ecb5849c3eded0615054041e1b723
sheey <sheey11@qq.com>
2020-12-07 12:45:17 +0800

    :bug: fixed several bugs

--- src/components/ShowViewNoteCard.vue
+++ src/components/ShowViewNoteCard.vue
@@ -4,13 +4,13 @@
     .home-note-head
       .home-note-title
         i#home-note-icon.el-icon-notebook-2
-        span {{this.noteTitle}}
+        span {{ this.noteTitle }}
     .line
     .home-note-info
       .home-note-info-warp
-      span.home-note-content 作者: {{this.noteAuthor}}
-      span.home-note-content 日期: {{this.noteDate}}
-      span#content.home-note-content 概要: {{this.noteMd}}
+      span.home-note-content 作者: {{ this.noteAuthor }}
+      span.home-note-content 日期: {{ this.noteDate }}
+      span#content.home-note-content 概要: {{ this.noteMd }}
 </template>
 
 <script>
@@ -22,7 +22,6 @@ export default {
       noteTitle: "",
       noteDate: "",
       noteMd: "",
-      noteId: "",
       noteAuthor: "",
     };
   },
@@ -30,17 +29,15 @@ export default {
     "id": String
   },
   created() {
-    Vue.$axios
-    .get(Vue.$composeUrl(Vue.$baseUrl, '/notes/' + this.id),{
-                         headers: Vue.$getAuthorizedHeader()
+    Vue.$axios.get(Vue.$composeUrl(Vue.$baseUrl, '/notes/' + this.id),{
+      headers: Vue.$getAuthorizedHeader()
     })
     .then((res)=>{
       this.noteTitle = res.data.title
       this.noteMd = res.data.content.substring(0,50)+"......"
       this.noteDate = res.data.createdAt.substring(0,10)
       this.noteAuthor = res.data.author.username
-      this.noteId = res.data.id
-      if(this.noteTitle.length>14){
+      if(this.noteTitle.length > 14){
         this.noteTitle = this.noteTitle.substring(0,12)+"......"
       }
     })
@@ -49,7 +46,9 @@ export default {
     })
   },
   methods: {
-    ClicknoteUrl() {this.$router.push("/viewnote/"+this.noteId)}
+    ClicknoteUrl() {
+      this.$router.push("/viewnote/" + this.id);
+    }
   }
 };
 </script>
--- src/views/Info.vue
+++ src/views/Info.vue
@@ -5,37 +5,34 @@ div.info
       Header
     template(slot="aside")
       div.side
-        el-menu(default-active="2"
-                class="el-menu-vertical-demo"
-                @open="handleOpen"
-                @close="handleClose")
+        el-menu(default-active="2")
           el-submenu(index="1")
             template(slot="title")
               i(class="el-icon-document")
               span() 笔记
             el-menu-item-group()
-              el-menu-item(index="1-1" @click="showNote()") 我的笔记
-              el-menu-item(index="1-2"  @click="showStartedNote()") 我的收藏
+              el-menu-item(index="1-1" @click="() => switchTo('notes')") 我的笔记
+              el-menu-item(index="1-2"  @click="() => switchTo('starednotes')") 我的收藏
           el-submenu(index = "2" )
             template(slot = "title")
               i(class="el-icon-user")
               span() 关注与粉丝
             el-menu-item-group()
-              el-menu-item(index="2-1" @click="showFollowee()") 关注
-              el-menu-item(index="2-2" @click="showFollower()") 粉丝
-          el-menu-item(index = "3" @click="showSetting()")  
-            i(class = "el-icon-setting") 
+              el-menu-item(index="2-1" @click="() => switchTo('followees')") 关注
+              el-menu-item(index="2-2" @click="() => switchTo('follewers')") 粉丝
+          el-menu-item(index = "3" @click="() => switchTo('settings')")
+            i(class = "el-icon-setting")
             span() 设置
     template(slot="main")
-      div(v-if="this.Note")
+      div(v-if="this.$route.params.tab=='notes'")
         MyNoteCard
-      div(v-else-if="this.StaredNote")
+      div(v-else-if="this.$route.params.tab=='starednotes'")
         SartedNoteCard
-      div(v-else-if="this.Follower")
+      div(v-else-if="this.$route.params.tab=='follewers'")
         FollowersCard
-      div(v-else-if="this.Followee")
+      div(v-else-if="this.$route.params.tab=='followees'")
         FolloweesCard
-      div(v-else-if="this.Setting").setting
+      div(v-else-if="this.$route.params.tab=='settings'").setting
         SettingCard
       div(v-else)
         MyNoteCard
@@ -53,7 +50,8 @@ import SettingCard from "@/components/SettingCard"
 import SartedNoteCard from "@/components/SartedNoteCard"
 import FollowersCard from "@/components/FollowersCard"
 import FolloweesCard from "@/components/FolloweesCard"
-import Vue from "vue";
+// import Vue from "vue";
+
 export default {
   name: "Info",
   components: {
@@ -68,55 +66,18 @@ export default {
     Header,
   },
   created(){
-    if (Vue.$jwt.get() == undefined || Vue.$jwt.get() == null){
-      this.$message.error("你还未登录");
-      this.$router.push("/login")
+    if(!this.$route.params.uid){
+      this.$router.push("/home")
     }
   },
   methods:{
-    showNote(){
-      this.Note = true;
-      this.StaredNote =false,
-      this.Setting = false,
-      this.Follower =false,
-      this.Followee =false
-    },
-    showSetting(){
-      this.Note = false;
-      this.StaredNote =false,
-      this.Setting = true,
-      this.Follower =false,
-      this.Followee =false
-    },
-    showStartedNote(){
-      this.Note = false;
-      this.StaredNote =true,
-      this.Setting = false,
-      this.Follower =false,
-      this.Followee =false
-    },
-    showFollower(){
-      this.Note = false;
-      this.StaredNote =false,
-      this.Setting = false,
-      this.Follower =true,
-      this.Followee =false
-    },
-    showFollowee(){
-      this.Note = false;
-      this.StaredNote =false,
-      this.Setting = false,
-      this.Follower =false,
-      this.Followee =true
+    switchTo(path){
+      this.$router.push(`/info/${this.$route.params.uid}/${path}`);
     }
   },
   data() {
     return{
-      Note: false,
-      StaredNote:false,
-      Setting: false,
-      Follower :false,
-      Followee :false,
+
     }
   },
 };
